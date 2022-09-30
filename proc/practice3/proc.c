6efc6addefad424be22a6cb0d37b19d800b2ce28
lrf <465197183@qq.com>
2022-09-30 23:52:25 +0800

    环境变量和命令行参数

--- proc/practice3/proc.c
+++ proc/practice3/proc.c
@@ -1,11 +1,62 @@
 #include <stdio.h>
 #include <unistd.h>
-int main()
+#include <string.h>
+
+
+int main(int argc, char* argv[])
 {
-	while(1)
-	{	
-		fork();
-		printf("i am a process\n");
+	if(argc != 2)
+	{
+		printf("Usage: %s -[a/b]\n",argv[0]);
+		return 1;
+	}
+
+	if(strcmp(argv[1], "-a") == 0)
+	{
+		printf("hello ^_^\n");
+	}
+	else if (strcmp(argv[1], "-b") == 0) 
+	{
+		printf("hello ^_&\n");
+	}
+	else
+	{
+		printf("&_&\n");
 	}
 	return 0;
 }
+
+// int main(int argc, char * argv[])
+// {
+// 	for(int i = 0;i < argc; i++)
+// 	{
+// 		printf("argv[%d] -> %s\n",i, argv[i]);
+// 	}
+
+// 	return 0;
+
+// }
+
+// int main()
+// {
+// 	int cnt = 5;
+// 	while(cnt)
+// 	{
+// 		sleep(1);
+// 		printf("i am a cmd -> process!\n");
+// 		cnt--;
+// 	}
+// 	return 0;
+// }
+
+
+
+// int main()
+// {
+// 	while(1)
+// 	{	
+// 		fork();
+// 		printf("i am a process\n");
+// 	}
+// 	return 0;
+// }
